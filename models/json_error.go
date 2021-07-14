9ff11c05e033d1ddb33099ecffd89cfddeaae1e7
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-07-14 15:33:34 +0000

    chore: Regenerate OpenAPI client v1.10.3
    Version: v1.10.3

--- models/json_error.go
+++ models/json_error.go
@@ -6,58 +6,48 @@ package models
 // Editing this file might prove futile when you re-run the swagger generate command
 
 import (
-	"github.com/go-openapi/errors"
+	"context"
+
 	"github.com/go-openapi/strfmt"
 	"github.com/go-openapi/swag"
-	"github.com/go-openapi/validate"
 )
 
-// GenericError Error response
+// JSONError Generic Error Response
 //
 // Error responses are sent when an error (e.g. unauthorized, bad request, ...) occurred.
 //
-// swagger:model genericError
-type GenericError struct {
-
-	// Debug contains debug information. This is usually not available and has to be enabled.
-	Debug string `json:"debug,omitempty"`
+// swagger:model jsonError
+type JSONError struct {
 
 	// Name is the error name.
-	// Required: true
-	Error *string `json:"error"`
+	// Example: The requested resource could not be found
+	Error string `json:"error,omitempty"`
+
+	// Debug contains debug information. This is usually not available and has to be enabled.
+	// Example: The database adapter was unable to find the element
+	ErrorDebug string `json:"error_debug,omitempty"`
 
 	// Description contains further information on the nature of the error.
+	// Example: Object with ID 12345 does not exist
 	ErrorDescription string `json:"error_description,omitempty"`
 
 	// Code represents the error status code (404, 403, 401, ...).
+	// Example: 404
 	StatusCode int64 `json:"status_code,omitempty"`
 }
 
-// Validate validates this generic error
-func (m *GenericError) Validate(formats strfmt.Registry) error {
-	var res []error
-
-	if err := m.validateError(formats); err != nil {
-		res = append(res, err)
-	}
-
-	if len(res) > 0 {
-		return errors.CompositeValidationError(res...)
-	}
+// Validate validates this json error
+func (m *JSONError) Validate(formats strfmt.Registry) error {
 	return nil
 }
 
-func (m *GenericError) validateError(formats strfmt.Registry) error {
-
-	if err := validate.Required("error", "body", m.Error); err != nil {
-		return err
-	}
-
+// ContextValidate validates this json error based on context it is used
+func (m *JSONError) ContextValidate(ctx context.Context, formats strfmt.Registry) error {
 	return nil
 }
 
 // MarshalBinary interface implementation
-func (m *GenericError) MarshalBinary() ([]byte, error) {
+func (m *JSONError) MarshalBinary() ([]byte, error) {
 	if m == nil {
 		return nil, nil
 	}
@@ -65,8 +55,8 @@ func (m *GenericError) MarshalBinary() ([]byte, error) {
 }
 
 // UnmarshalBinary interface implementation
-func (m *GenericError) UnmarshalBinary(b []byte) error {
-	var res GenericError
+func (m *JSONError) UnmarshalBinary(b []byte) error {
+	var res JSONError
 	if err := swag.ReadJSON(b, &res); err != nil {
 		return err
 	}
